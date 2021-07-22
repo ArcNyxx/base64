e671aeac03777cc6da2a4af668bb8e7865d4aef2
Doggo <fearlessdoggo21@vivaldi.net>
2021-07-21 20:23:45 -0500

    Overhaul testing with escape codes for prettiness

--- test.c
+++ test.c
@@ -34,38 +34,79 @@
 #include "base64.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+void DiffChars(const char *string, const char *compare, const size_t length) {
+    if (!length) {
+        return;
+    }
+    char *swapCode = "\x1b[31m";
+    char correct = *string == *compare, newCorrect;
+    printf("\x1b[3%cm%c", '1' + correct, *string);
+
+    for (size_t i = 1; i < length; i++) {
+        newCorrect = string[i] == compare[i];
+        if (correct != newCorrect) {
+            printf("\x1b[3%cm", '1' + newCorrect);
+        }
+        printf("%c", string[i]);
+        
+        correct = newCorrect;
+    }
+
+    printf("\x1b[m\n");
+}
+
 int main() {
-    char *string = "NXBUwl^MYZVJi47a1*n22@iTCD-%Z3rM&YJoLU&9Ss6Y_T5*sW?ufI27iKN"
+    const char *testDec = "NXBUwl^MYZVJi47a1*n22@iTCD-%Z3rM&YJoLU&9Ss6Y_T5*sW?ufI27iKN"
     "4Lst%@sA2N7&IX9dXl-D&^-A-B$7=i9dDNdg7ygcK#KBFT?A9T&h|k#=l=3699Rp1pPhJO+fx="
     "fdD^b0s%$i?EPBq$mqX9n3xr*OEMk3*!dbHu=Uy5nc3O9U3h4rCW&lk$EkZ!@XD6*d0Bj#==qb"
     "CnW*g=E0r^ItOIizy0SdMe32xh9xupKZsRwx7hj-YG!Wpc$d^";
+    
+    const char *testEnc = "TlhCVXdsXk1ZWlZKaTQ3YTEqbjIyQGlUQ0QtJVozck0mWUpvTFUmOVNzNll"
+    "fVDUqc1c_dWZJMjdpS040THN0JUBzQTJONyZJWDlkWGwtRCZeLUEtQiQ3PWk5ZEROZGc3eWdjS"
+    "yNLQkZUP0E5VCZofGsjPWw9MzY5OVJwMXBQaEpPK2Z4PWZkRF5iMHMlJGk_RVBCcSRtcVg5bjN"
+    "4cipPRU1rMyohZGJIdT1VeTVuYzNPOVUzaDRyQ1cmbGskRWtaIUBYRDYqZDBCaiM9PXFiQ25XK"
+    "mc9RTByXkl0T0lpenkwU2RNZTMyeGg5eHVwS1pzUnd4N2hqLVlHIVdwYyRkXg==";
 
-    char *encode;
-    size_t encodedLen = GetEncodedLength(string);
-    if (!(encode = malloc(1 + encodedLen))) {
-        return 1;
+    size_t lenEnc = GetEncodedLength(testDec);
+    size_t lenDec = GetDecodedLength(testEnc);
+
+    char encode[lenEnc + 1];
+    EncodeBase64(testDec, lenDec, encode);
+    encode[lenEnc] = 0;
+
+    char verify = VerifyBase64(encode, lenEnc);
+    printf("Encoded string \x1b[1m%s\x1b[m valid.\n", verify ? "is" : "is not");
+
+    char encoded = strcmp(encode, testEnc);
+    printf("Encoded string \x1b[1m%s\x1b[m correct.\n", encoded ? "is not" : "is");
+    if (encoded) {
+        // Testing for DiffChars function
+        // memcpy(encode + 12, "aweouhwaeunaeriuanero", 21);
+        // memcpy(encode + 90, "w4ouyJwnnavriaweh", 17);
+
+        DiffChars(encode, testEnc, lenEnc);
+        printf("%s\n", testEnc);
     }
-    encode[encodedLen] = '\0';
 
-    EncodeBase64(string, strlen(string), encode);
-    printf("Encoded:\n%s\n", encode);
+    char decode[lenDec + 1];
+    DecodeBase64(testEnc, lenEnc, decode);
+    decode[lenDec] = 0;
 
-    char verified = VerifyBase64(encode, encodedLen);
-    printf("Verified:\n%s\n", verified ? "True" : "False");
-
-    char *decode;
-    size_t decodedLen = GetDecodedLength(encode);
-    if (!(decode = malloc(1 + decodedLen))) {
-        return 1;
+    char decoded = strcmp(decode, testDec);
+    printf("Decoded string \x1b[1m%s\x1b[m correct.\n", decoded ? "is not" : "is");
+    if (decoded) {
+        DiffChars(decode, testDec, lenDec);
+        printf("%s\n", testDec);
     }
-    decode[decodedLen] = '\0';
-
-    DecodeBase64(encode, encodedLen, decode);
-    printf("Decoded:\n%s\n", decode);
-    printf("Original:\n%s\n", string);
-    printf("Correct:\n%s\n", strcmp(string, decode) ? "No" : "Yes");
+    
+    if (verify && !encoded && !decoded) {
+        printf("\x1b[1m\x1b[32mTesting success!\x1b[m\n");
+    } else {
+        printf("\x1b[1m\x1b[31mTesting failure!\x1b[m\n");
+    }
 
     return 0;
 }
